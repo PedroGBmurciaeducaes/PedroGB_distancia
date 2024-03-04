5d7248253be84d6471421e560759d0abefdae4ab
PedroGBmurciaeducaes <calav@Pedro>
2024-03-04 14:12:15 +0100

    Cambios para parte 4 del ejercicio

---
+++ ProyectoGitHub/src/main/java/com/mycompany/proyectogithub/ProyectoGitHub.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.proyectogithub;
+
+
+public class ProyectoGitHub {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hola que tal");
+    }
+}
